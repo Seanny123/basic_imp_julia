94160a1dbf4b60d2e0367dbbabfcd37460123398
Sean Aubin <seanaubin@gmail.com>
2019-01-15 17:32:54 -0500

    removed out-dated comment

--- utils.jl
+++ utils.jl
@@ -4,11 +4,11 @@ using ..Structs: Query
 
 export dist
 
-# removing type annotations allows the program
-# to run without errors
+
 function dist(x::Query, y::Query)
     return (x.data - y.data) ^ 2
 end
+
 
 module MidDist
     export mid_dist
