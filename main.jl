8e293116cddcc0d2fa171ca7093f2a1f065a37e0
Sean Aubin <seanaubin@gmail.com>
2019-01-15 17:31:55 -0500

    added function inside of module

--- main.jl
+++ main.jl
@@ -3,8 +3,10 @@ include("utils.jl")
 
 using .Structs: Query
 using .SimpleDist: dist
+using .SimpleDist.MidDist: mid_dist
 
 a = Query("a", 1)
 b = Query("b", -1)
 
 println(dist(a, b))
+println(mid_dist(a, b))
--- utils.jl
+++ utils.jl
@@ -10,4 +10,12 @@ function dist(x::Query, y::Query)
     return (x.data - y.data) ^ 2
 end
 
+module MidDist
+    export mid_dist
+
+    function mid_dist(x, y)
+        return ((x.data - y.data) ^ 2) / 2
+    end
+end
+
 end
